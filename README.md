e2690ffb7a0309a8b37667f9bcea85f94490174d
unknown <Per Westling>
2015-10-18 21:46:04 +0200

    First version after taking SunshinePer and change to PopularMovies

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # PopularMovies
-Project done as part of Udacity's nanodegree course in Adroind development.-
+Project done as part of Udacity's nanodegree course in Adroind development.
+
+# API Key
+To build, and deploy this, you need to add an API key. This file is ignored
+by git to avoid it being checked in.
+The API key xml file is in the following format:
+--- BEGIN ---
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <string name="api_key" translatable="false">apikey</string>
+</resources>
+--- END ---
+To use this file place it as app/src/main/res/values/api_key.xml
+and replace the value "apikey" above with your API key.
+Note! Do NOT commit in this file.
---
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.android.popularmovies;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        if (id == R.id.action_map) {
+            showMap();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showMap() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String value = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        String location = value.replaceAll(",", "+");
+
+        Uri geoLocation = Uri.parse("geo:0,0?q=" + location);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+}
